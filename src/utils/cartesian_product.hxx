c7748ca8ecd4bb9529c8abebf7d8e869ae90da18
Guillem Frances <guillem.frances@gmail.com>
2015-05-13 20:30:00 +0200

    small bugfix

--- src/utils/cartesian_product.hxx
+++ src/utils/cartesian_product.hxx
@@ -13,30 +13,37 @@ class CartesianProductIterator {
 protected:
 	const DomainVector& _values;
 	std::vector<Domain::const_iterator> _iterators;
+	std::vector<ObjectIdx> _element; // The current element
 	bool _ended;
 
 public:
 	CartesianProductIterator(const DomainVector& values) :
 		_values(values),
 		_iterators(),
+		_element(),
 		_ended(values.size() == 0)
 	{
 		// Initialize the iterator vector and check that all the sets of the cartesian product have at least one element
 		// (otherwise the product will be empty)
+		_iterators.reserve(_values.size());
+		_element.reserve(_values.size());
 		for (const DomainPtr& domain:values) {
 			if (domain->size() == 0) {
 				_ended = true;
 				break;
 			}
-			_iterators.push_back(domain->begin());
+			auto it = domain->begin();
+			_iterators.push_back(it);
+			_element.push_back(*it);
 		}
 	}
 	
 	//! Advances the iterator at position 'idx' or, if it has reached the end, resets its and tries with the one at the left, recursively.
 	void advanceIterator(unsigned idx) {
 		assert(idx < _iterators.size());
+		++_iterators[idx];
 		if (_iterators[idx] != _values[idx]->end()) {
-			++_iterators[idx];
+			updateElement(idx);
 		} else {
 			if (idx == 0) { // Base case: We're done with all the elements in the cartesian product.
 				_ended = true;
@@ -45,20 +52,18 @@ public:
 			
 			// otherwise: reset the current idx to zero and try incrementing the previous one.
 			_iterators[idx] = _values[idx]->begin();
+			updateElement(idx);
 			advanceIterator(idx-1);
 		}
 	}
+	
+	inline void updateElement(unsigned idx) {
+		assert(_iterators[idx] != _values[idx]->end());
+		_element[idx] = *(_iterators[idx]);
+	}
 
 public:
-	std::vector<ObjectIdx> operator*() const {
-		// Build a point with the current indexes.
-		std::vector<ObjectIdx> element;
-		element.reserve(_iterators.size());
-		for (const auto& position:_iterators) {
-			element.push_back(*position); // Dereference the iterator.
-		}
-		return element;
-	}
+	const std::vector<ObjectIdx>& operator*() const { return _element; }
 	
 	const CartesianProductIterator& operator++() {
 		advanceIterator(_iterators.size()-1);
