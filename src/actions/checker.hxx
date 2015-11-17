4b4050ccad3de4987b8a650d1c04963d79930e38
Guillem Frances <guillem.frances@gmail.com>
2015-11-17 17:33:46 +0100

    small refactoring of plan checking

--- src/actions/checker.hxx
+++ src/actions/checker.hxx
@@ -17,12 +17,6 @@ public:
 	
 	//! Returns true iff the given lifted-action plan is valid and leads to a goal state when applied to s0
 	static bool check_correctness(const Problem& problem, const std::vector<LiftedActionID>& plan, const State& s0);
-	
-	template <typename ActionT>
-	static bool check_correctness(const Problem& problem, const std::vector<ActionT>& plan, std::function<const GroundAction&(const ActionT&)> getter, const State& s0);
-	
-	//! "Materialize" a lifted-action plan into a sequence of ground actions
-	void derive_plan(const std::vector<LiftedActionID>& plan);
 };
 
 
