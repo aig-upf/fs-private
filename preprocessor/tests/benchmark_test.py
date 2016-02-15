96c6b3f6ba3b7a52933943d8e1d559c0922b7223
Guillem Frances <guillem.frances@gmail.com>
2016-02-15 18:29:49 +0100

    Preprocessor tests - cosmetic changes

--- preprocessor/tests/benchmark_test.py
+++ preprocessor/tests/benchmark_test.py
@@ -1,3 +1,8 @@
+"""
+ This test suites performs an across-the-board compilation for a number of selected instances.
+ Expensive, but necessary.
+"""
+
 import os
 import generator
 
@@ -20,22 +25,22 @@ instances = [
 def pytest_generate_tests(metafunc):
     # This is called once for each test method, and allows us to pass different parameters to the test method,
     # in this case as many instances as desired, so that each instance is mapped into a different test.
-    if metafunc.cls != TestBenchmarks:
+    # @see http://pytest.org/latest/example/parametrize.html#parametrizing-test-methods-through-per-class-configuration
+    if metafunc.function != test_instance:
         return
     argnames = ['instance']
     argvalues = [[inst] for inst in instances]
     metafunc.parametrize(argnames, argvalues)
 
 
-class TestBenchmarks(object):
-    def test_instance(self, instance):
-        """
-        Run the whole preprocessor on the given problem instance.
-        :param instance: The planning problem instance
-        :return:
-        """
-        assert benchmarks
-        filename = os.path.join(benchmarks, 'benchmarks', instance)
-        args = ['--tag', 'pytest', '--edebug', '--instance', filename]
-        args = generator.parse_arguments(args)
-        generator.main(args)
+def test_instance(instance):
+    """
+    Run the whole preprocessor on the given problem instance.
+    :param instance: The planning problem instance
+    :return:
+    """
+    assert benchmarks
+    filename = os.path.join(benchmarks, 'benchmarks', instance)
+    args = ['--tag', 'pytest', '--edebug', '--instance', filename]
+    args = generator.parse_arguments(args)
+    generator.main(args)
