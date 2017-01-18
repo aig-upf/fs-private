3a1bb9dd00dbcff6a6fc7f4f2b81495031c9a443
Guillem Frances <guillem.frances@gmail.com>
2017-01-18 14:43:41 +0100

    proper use of boost::dynamic_bitset

--- src/state.hxx
+++ src/state.hxx
@@ -2,16 +2,8 @@
 #pragma once
 
 #include <fs_types.hxx>
+#include <utils/bitsets.hxx>
 
-#define BOOST_DYNAMIC_BITSET_DONT_USE_FRIENDS // see http://stackoverflow.com/a/3897217
-#include <boost/dynamic_bitset.hpp>
-
-namespace boost {
-    template <typename B, typename A>
-    std::size_t hash_value(const boost::dynamic_bitset<B, A>& bs) {
-        return boost::hash_value(bs.m_bits);
-    }
-}
 
 namespace fs0 {
 
