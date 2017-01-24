55d5cbeae6907359255c3b6f8574937bb81c9729
Guillem Frances <guillem.frances@gmail.com>
2017-01-24 17:48:05 +0100

    allow running simulation in macro-mode, in which the simulation is used to compute a set of macro actions that are used to expand the node, disregarding set of relevant atoms

--- src/search/drivers/sbfws/base.cxx
+++ src/search/drivers/sbfws/base.cxx
@@ -7,12 +7,15 @@ namespace fs0 { namespace bfws {
 SBFWSConfig::SBFWSConfig(const Config& config) :
 	search_width(config.getOption<int>("width.search")),
 	simulation_width(config.getOption<int>("width.simulation")),
-	mark_negative_propositions(config.getOption<bool>("relevance.neg_prop"))
+	mark_negative_propositions(config.getOption<bool>("relevance.neg_prop")),
+	complete_simulation(config.getOption<bool>("relevance.complete", true))
 {
-	relevant_set_type = RelevantSetType::Sim;
 	std::string rs = config.getOption<std::string>("bfws.rs");
 	if (rs == "hff") relevant_set_type = RelevantSetType::HFF;
 	else if  (rs == "aptk_hff") relevant_set_type = RelevantSetType::APTK_HFF;
+	else if  (rs == "macro") relevant_set_type = RelevantSetType::Macro;
+	else if  (rs == "sim") relevant_set_type = RelevantSetType::Sim;
+	else throw std::runtime_error("Unknown option value \"bfws.rs\"=" + rs);
 }
 
 } } // namespaces
