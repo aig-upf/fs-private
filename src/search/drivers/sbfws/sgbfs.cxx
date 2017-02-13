3a5e8deb346fae65843761f73dffaf840b0cce8b
Guillem Frances <guillem.frances@gmail.com>
2017-02-13 22:46:07 +0100

    use extra features only optionally

--- src/search/drivers/sbfws/sgbfs.cxx
+++ src/search/drivers/sbfws/sgbfs.cxx
@@ -71,13 +71,16 @@ LazyBFWSDriver<StateModelT>::do_search(const StateModelT& model, const Config& c
 	const StateAtomIndexer& indexer = model.getTask().getStateAtomIndexer();
 	
 	
-	FeatureSelector<StateT> selector(ProblemInfo::getInstance());
+	if (config.getOption<bool>("bfws.extra_features", false)) {
+		FeatureSelector<StateT> selector(ProblemInfo::getInstance());
+		if (selector.has_extra_features()) {
+			LPT_INFO("cout", "FEATURE EVALUATION: Extra Features were found!  Using a GenericFeatureSetEvaluator");
+			using FeatureEvaluatorT = lapkt::novelty::GenericFeatureSetEvaluator<StateT>;
+			return do_search2<FSMultivaluedNoveltyEvaluatorI, FeatureEvaluatorT>(model, selector.select(), config, out_dir, start_time);
+		}
+	}
 	
-	if (selector.has_extra_features()) {
-		LPT_INFO("cout", "FEATURE EVALUATION: Extra Features were found!  Using a GenericFeatureSetEvaluator");
-		using FeatureEvaluatorT = lapkt::novelty::GenericFeatureSetEvaluator<StateT>;
-		return do_search2<FSMultivaluedNoveltyEvaluatorI, FeatureEvaluatorT>(model, selector.select(), config, out_dir, start_time);
-	} else if (indexer.is_fully_binary()) { // The state is fully binary
+	if (indexer.is_fully_binary()) { // The state is fully binary
 		LPT_INFO("cout", "FEATURE EVALUATION: Using the specialized StraightFeatureSetEvaluator<bin>");
 		using FeatureEvaluatorT = lapkt::novelty::StraightFeatureSetEvaluator<bool>;
 		return do_search2<FSBinaryNoveltyEvaluatorI, FeatureEvaluatorT>(model, FeatureEvaluatorT(), config, out_dir, start_time);
