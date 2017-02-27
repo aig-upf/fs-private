a2390a64b9b65764a7f3fe57cc331ba0bd79358d
Guillem Frances <guillem.frances@gmail.com>
2017-02-27 19:49:31 +0100

    build script

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
-module load gcc/6.2.0
+command -v module >/dev/null 2>&1 && module load gcc/6.2.0
 
-./build.py
+cd $LAPKT && rm -rf include lib
+cd $LAPKT2_PATH && ./build.py && ./install.py
+cd $LAPKT_NOVELTY_PATH && ./build.py && ./install.py
+
+cd $FS_PATH && ./build.py
