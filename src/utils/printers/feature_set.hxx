32087ecc009f35b4c3d4a36a601ae1c13d735712
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2017-03-02 14:31:00 +1100

    Updated include path

--- src/utils/printers/feature_set.hxx
+++ src/utils/printers/feature_set.hxx
@@ -4,19 +4,19 @@
 #include <vector>
 #include <ostream>
 #include <fs_types.hxx>
-#include <lapkt/heuristics/novelty/tuples.hxx>
+#include <lapkt/novelty/tuples.hxx>
 
 namespace fs0 { namespace print {
 
 class feature_set {
 	protected:
 		std::vector<aptk::VariableIndex>& _varnames;
-		
+
 		std::vector<aptk::ValueIndex>& _values;
-		
+
 	public:
 		feature_set(std::vector<aptk::VariableIndex>& varnames, std::vector<aptk::ValueIndex>& values) : _varnames(varnames), _values(values) {}
-		
+
 		friend std::ostream& operator<<(std::ostream &os, const feature_set& o) { return o.print(os); }
 		std::ostream& print(std::ostream& os) const;
 };
