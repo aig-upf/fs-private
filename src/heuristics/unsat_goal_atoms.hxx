6830cc6ca289ef2ff45b54ffae8e93253bb1ba59
Guillem Frances <guillem.frances@gmail.com>
2017-03-13 17:04:39 +0100

    The "number of unsat goal atoms" heuristic now works as well for non-conjunctive goals; simply wrapping them up into a "dummy" conjunction with one single conjunct"

--- src/heuristics/unsat_goal_atoms.hxx
+++ src/heuristics/unsat_goal_atoms.hxx
@@ -9,7 +9,7 @@ namespace fs0 {
 //! The heuristic value of any given state is the number of unsatisfied goal conditions (atoms) on that state
 class UnsatisfiedGoalAtomsHeuristic {
 public:
-	UnsatisfiedGoalAtomsHeuristic(const Problem& problem) : _goal_conjunction(extract_goal_conjunction_or_fail(problem)) {}
+	UnsatisfiedGoalAtomsHeuristic(const Problem& problem) : _goal_conjunction(extract_goal_conjunction(problem)) {}
 	
 	//! The actual evaluation of the heuristic value for any given non-relaxed state s.
 	float evaluate(const State& state) const { 
@@ -26,11 +26,14 @@ public:
 	
 	
 protected:
-	const fs::Conjunction* _goal_conjunction;
+	const std::unique_ptr<const fs::Conjunction> _goal_conjunction;
 	
-	const fs::Conjunction* extract_goal_conjunction_or_fail(const Problem& problem) {
-		auto goal_conjunction = dynamic_cast<const fs::Conjunction*>(problem.getGoalConditions());
-		if (!goal_conjunction) throw std::runtime_error("UnsatisfiedGoalAtomsHeuristic valid only if the goal is a conjunction of atoms");
+	const fs::Conjunction* extract_goal_conjunction(const Problem& problem) {
+		auto clone = problem.getGoalConditions()->clone();
+		auto goal_conjunction = dynamic_cast<const fs::Conjunction*>(clone);
+		if (!goal_conjunction) { // If the goal is now inside a conjunction, we wrap it up inside a new one.
+			goal_conjunction = new fs::Conjunction({clone});
+		}
 		return goal_conjunction;
 	}
 };
