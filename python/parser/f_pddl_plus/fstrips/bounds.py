b8afda7b388bf9e6ed562edc74e04265dafb0bed
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2017-04-21 11:43:25 +1000

    AST Refactoring
     * Removed old classes from f-PDDL+ parser

--- python/parser/f_pddl_plus/fstrips/bounds.py
+++
@@ -1,6 +0,0 @@
-class DomainBound(object):
-    def __init__(self, typename, bound):
-        self.typename = typename
-        self.bound = bound
-    def __str__(self):
-        return "{} - {}".format(self.typename, self.bound)
--- python/parser/f_pddl_plus/fstrips/metrics.py
+++
@@ -1,8 +0,0 @@
-
-class Metric : # The problem metric
-    def __init__(self, optimization) :
-        self.optimization = optimization
-        self.expr = None
-
-    def __str__(self) :
-        return "{} {}".format(self.optimization, self.expr)
