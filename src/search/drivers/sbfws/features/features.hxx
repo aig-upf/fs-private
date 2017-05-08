572b98d0ce0aaed53b042187db54fa2f9f00c37f
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2017-05-08 11:27:45 +1000

    State Variable Projection for Novelty Evaluation
     * Added the ability to filter out specific state variables (i.e. clock_time()) from
       the evaluation of novelty, this requires at the moment specifying the options:
    
         bfws.extra_features=true,project_away_time=true

--- src/search/drivers/sbfws/features/features.hxx
+++ src/search/drivers/sbfws/features/features.hxx
@@ -14,17 +14,19 @@ class FeatureSelector {
 public:
 	using EvaluatorT = lapkt::novelty::GenericFeatureSetEvaluator<StateT>;
 	using FeatureT = typename EvaluatorT::FeatureT;
-	
+
 	FeatureSelector(const ProblemInfo& info) : _info(info) {}
-	
+
 	bool has_extra_features() const;
-	
+
+    bool projecting_away_vars() const;
+
 	EvaluatorT select();
-	
+
 	void add_state_variables(const ProblemInfo& info, std::vector<FeatureT*>& features);
-	
+
 	void add_extra_features(const ProblemInfo& info, std::vector<FeatureT*>& features);
-	
+
 protected:
 	const ProblemInfo& _info;
 };
