235d2ceb09531aa9eef05e0f2c2251f0a31a55a6
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2017-11-21 14:39:32 +1100

    Removed extra '/' from #include

--- src/fs/core/languages/fstrips/base.hxx
+++ src/fs/core/languages/fstrips/base.hxx
@@ -3,13 +3,13 @@
 
 #include <iostream>
 
-#include <fs/core/utils//visitor.hxx>
+#include <fs/core/utils/visitor.hxx>
 
 namespace fs0 { class ProblemInfo; } // TODO[LAMBDA] - REMOVE THIS DEPENDENCY
 
 namespace fs0 { namespace language { namespace fstrips {
 
-	
+
 //! A common base class for both terms and formulas
 class LogicalElement :
  	public Loki::BaseVisitable<void, Loki::ThrowCatchAll, true>
@@ -18,14 +18,13 @@ class LogicalElement :
 {
 public:
 	virtual ~LogicalElement() = default;
-	
+
 	//! Clone idiom
 	virtual LogicalElement* clone() const = 0;
-	
+
 	//! Prints a representation of the object to the given stream.
 	friend std::ostream& operator<<(std::ostream &os, const LogicalElement& o);
 	virtual std::ostream& print(std::ostream& os, const fs0::ProblemInfo& info) const = 0;
 };
 
 } } } // namespaces
-
