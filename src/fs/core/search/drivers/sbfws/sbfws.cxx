15e38f3a0b2d4e58b1dfae4023f81ca4ba7333f8
Guillem Frances <guillem.frances@gmail.com>
2018-01-22 14:48:02 +0100

    minor fix

--- src/fs/core/search/drivers/sbfws/sbfws.cxx
+++ src/fs/core/search/drivers/sbfws/sbfws.cxx
@@ -82,7 +82,8 @@ ExitCode
 SBFWSDriver<StateModelT>::do_search1(const StateModelT& model, FeatureEvaluatorT&& featureset, const Config& config, const drivers::EngineOptions& options, float start_time) {
 	SBFWSConfig bfws_config(config);
 
-    bool actionless = model.getTask().getPartiallyGroundedActions().empty();
+    bool actionless = model.getTask().getPartiallyGroundedActions().empty() &&
+            model.getTask().getGroundActions().empty();
 
     if ( bfws_config.using_feature_set) {
 		auto engine = create<StateModelT, FeatureEvaluatorT, NoveltyEvaluatorT, MultiValuedIWRun, MultiValuedIWRunNode>(std::move(featureset), bfws_config, model, _stats);
