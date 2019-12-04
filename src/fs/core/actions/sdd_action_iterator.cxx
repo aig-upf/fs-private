3c4b3aa836feacf9815705658b3cf2e9ab2dff7a
Guillem Frances <guillem.frances@gmail.com>
2019-12-04 20:06:32 +0100

    Remove old code that used conditioning before enumerating models

--- src/fs/core/actions/sdd_action_iterator.cxx
+++ src/fs/core/actions/sdd_action_iterator.cxx
@@ -39,50 +39,19 @@ namespace fs0 {
             if (!current_models_computed_) {
                 assert (current_resultset_.empty());
 
-                /*
-                if (!custom_me_) {
-                    // LPT_DEBUG("cout", "Conjoining SDD " << schema_sdd.get_schema().getName() << " with state...");
-                    // Create the SDD corresponding to the current action schema index conjoined with the current state.
-                    current_sdd_ = schema_sdd.conjoin_with(state_);
-                    if (!current_sdd_ ||
-                        sdd_node_is_false(current_sdd_)) { // no applicable ground action for this schema
-
-                        // No delete, as SDD library has custom mem management.
-                        // This should collect the garbage from current_sdd_ (i.e. closest thing to deleting the pointer),
-                        // as it will garbage-collect without referencing the current_sdd node
-                        schema_sdd.collect_sdd_garbage();
-                        current_sdd_ = nullptr;
-//                    LPT_DEBUG("cout", "Conjoined SDD has no groundings!");
-                        continue;
-                    }
-
-                    //                auto wmc_manager = wmc_manager_new(current_sdd_, 0, schema_sdd.manager());
-//                double wmc = wmc_propagate(wmc_manager);
-//                std::cout << "Action schema " << schema_sdd.get_schema().getName() << " has " << wmc << " models... " << std::flush;
-//                LPT_DEBUG("cout", "Conjoined SDD has " <<  sdd_size(current_sdd_) << " nodes");
-                }
-                */
-
-
                 // TODO custom_me no longer means custom me - fix this :-)
                 if (custom_me_) {
                     RecursiveModelEnumerator enumerator(schema_sdd.manager(), schema_sdd.collect_state_literals(state_));
                     current_resultset_ = enumerator.models(schema_sdd.node());
 
                 } else {
-                    // Just pass an empty SDDModel, i.e. with no value fixed
-//                    RecursiveModelEnumerator enumerator(schema_sdd.manager(), SDDModel(schema_sdd.var_count()+1));
-//                    current_resultset_ = enumerator.models(current_sdd_);
                     RecursiveModelEnumerator2 enumerator(schema_sdd.manager(), schema_sdd.collect_state_literals(state_));
                     current_resultset_ = enumerator.models(schema_sdd.node());
                 }
+//              std::cout << current_resultset_.size() << " models were actually retrieved" << std::endl;
 
                 current_models_computed_ = true;
-//                std::cout << current_resultset_.size() << " models were actually retrieved" << std::endl;
                 current_resultset_idx_ = 0;
-                // This should collect the garbage from current_sdd_ (i.e. closest thing to deleting the pointer),
-                // as it will garbage-collect without referencing the current_sdd node
-//                schema_sdd.collect_sdd_garbage(); // This doesnt make sense if we don't modify the SDD with data from the current state
             }
 
             if (current_resultset_idx_ < current_resultset_.size()) {
