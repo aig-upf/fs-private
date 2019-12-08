43f4a7a0cd78c250a9caf7c4e149403a2ab0d4ed
Guillem Frances <guillem.frances@gmail.com>
2019-12-08 19:20:10 +0100

    Some SDD experiments

---
+++ experiments/collect.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+from lab.experiment import Experiment
+from common import DEFAULT_ATTRIBUTES, BaseReport
+
+
+def main():
+
+    algos=['brfs-naive', 'brfs-sdd-asp', 'brfs-sdd-vo-asp']
+
+    exp = Experiment('/homedtic/gfrances/projects/code/fs-sdd/experiments/data/combined')
+    exp.add_fetcher('/homedtic/gfrances/projects/code/fs-sdd/experiments/data/medium-eval', filter_algorithm=algos)
+    exp.add_fetcher('/homedtic/gfrances/projects/code/fs-sdd/experiments/data/medium_sok_only-eval', filter_algorithm=algos)
+    exp.add_fetcher('/homedtic/gfrances/projects/code/fs-sdd/experiments/data/hard-eval', filter_algorithm=algos)
+
+    exp.add_report(
+        BaseReport(attributes=DEFAULT_ATTRIBUTES),
+        outfile='report.html')
+
+    # Parse the commandline and run the specified steps.
+    exp.run_steps()
+
+
+if __name__ == '__main__':
+    main()
