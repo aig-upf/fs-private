47e5c033c653deb7759bf059a0571360b61dc68b
Guillem Frances <guillem.frances@gmail.com>
2019-12-12 10:57:12 +0100

    More reporting

--- experiments/ipc18.py
+++ experiments/ipc18.py
@@ -5,7 +5,7 @@ import os
 from lab.experiment import Experiment
 from downward import suites
 from common import generate_environment, get_fsplanner_binary, add_all_runs, \
-    filter_benchmarks_if_test_run
+    filter_benchmarks_if_test_run, ALL_ATTRIBUTES
 
 TIME_LIMIT = 1800
 MEMORY_LIMIT = 8000
@@ -31,7 +31,7 @@ BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
 
 def algorithms():
     # We use Lab's data directory as workspace.
-    common = [get_fsplanner_binary(), '--domain', '{domain}', '-i', '{problem}', '--output', '.']
+    common = [get_fsplanner_binary(), '--domain', '{domain}', '-i', '{problem}', '--output', '.', '--asp']
 
     # Blind
     # ./run.py --asp -i /home/frances/projects/code/downward-benchmarks/agricola-sat18-strips/p01.pddl --driver sbfws --options="successor_generation=adaptive,evaluator_t=adaptive,bfws.rs=none"
@@ -55,12 +55,18 @@ def main():
     benchmarks = suites.build_suite(os.environ["DOWNWARD_BENCHMARKS"], SUITE)
     benchmarks = filter_benchmarks_if_test_run(benchmarks)
 
+    attributes = [a for a in ALL_ATTRIBUTES.keys() if not a.startswith('sdd')]
+
     add_all_runs(
         experiment=exp,
         suites=benchmarks,
         algorithms=algorithms(),
         time_limit=TIME_LIMIT,
-        memory_limit=MEMORY_LIMIT)
+        memory_limit=MEMORY_LIMIT,
+        attributes=attributes,
+    )
+
+    # exp.add_parse_again_step()
 
     # Parse the commandline and run the specified steps.
     exp.run_steps()
