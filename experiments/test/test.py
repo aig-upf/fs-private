806b32ab627f1cfd42e4d581afc091c2df74d491
Guillem Frances <guillem.frances@gmail.com>
2019-12-24 21:52:38 +0100

    Updating lab interfaces

---
+++ experiments/test/test.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+import os
+
+from fslab.common_setup import FSIssueExperiment, IssueConfig
+from common import generate_environment, filter_benchmarks_if_test_run, ALL_ATTRIBUTES, add_standard_experiment_steps
+
+TIME_LIMIT = 1800
+MEMORY_LIMIT = 8000
+
+SUITE = [
+    'agricola-sat18-strips',
+]
+REVISIONS = ["7a74ab08", "149bbb91"]
+BENCHMARKS_DIR = os.environ["DOWNWARD_BENCHMARKS"]
+
+
+def configs():
+    options = ['--asp', '--driver', 'sbfws', "--options", "verbose_stats=true,successor_generation=adaptive,evaluator_t=adaptive,bfws.rs=none"]
+    return [
+        IssueConfig("bfws-ray", options)
+    ]
+
+
+def main():
+    environment = generate_environment(email="guillem.frances@upf.edu",
+                                       memory_limit=MEMORY_LIMIT, time_limit=TIME_LIMIT)
+    exp = FSIssueExperiment(
+        revisions=REVISIONS,
+        configs=configs(),
+        environment=environment,
+        time_limit=TIME_LIMIT,
+        memory_limit=MEMORY_LIMIT
+    )
+
+    benchmarks = filter_benchmarks_if_test_run(SUITE)
+
+    attributes = [att for name, att in ALL_ATTRIBUTES.items() if not name.startswith('sdd')]
+
+    exp.add_suite(os.environ["DOWNWARD_BENCHMARKS"], benchmarks)
+
+    add_standard_experiment_steps(exp, add_parse_step=True)
+
+    exp.add_absolute_report_step(attributes=attributes)
+    # exp.add_comparison_table_step()
+
+    exp.run_steps()  # Let's go!
+
+
+if __name__ == '__main__':
+    main()
