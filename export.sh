ece2c0594d6f962213dd1b326ec5f5ffa4eb273d
Guillem Frances <guillem.frances@gmail.com>
2019-12-25 21:39:46 +0100

    Improving export script

--- export.sh
+++ export.sh
@@ -14,14 +14,43 @@ fi
 
 REVISION=$1
 OUTPUT_DIR=$2
-CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
-echo "> Exporting revision ${REVISION} to directory ${OUTPUT_DIR}"
-echo "> Exporting main repository"
+# the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Checkout the desired revision and update the submodules
-git stash
-git checkout ${REVISION}
+echo "Current dir is: $DIR"
+
+
+# the temp directory used, within $DIR
+# omit the -p parameter to create a temporal directory in the default location
+WORK_DIR=`mktemp -d`
+
+# check if tmp dir was created
+if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+else
+  echo "> Created temporary directory $WORK_DIR"
+fi
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$WORK_DIR"
+  echo "Deleted temp working directory $WORK_DIR"
+}
+
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+cp -r $DIR/. $WORK_DIR
+cd $WORK_DIR
+
+# Get rid of undesired changes
+git clean -fd
+git checkout -- .
+
+# Checkout the desired revision and update the submodules at that point in time
+git checkout -b export-branch ${REVISION}
 git submodule update
 
 # Export the revision to the desired directory
@@ -29,11 +58,4 @@ git archive HEAD | tar -x -C ${OUTPUT_DIR}
 
 # Export all submodules as well
 echo "> Exporting submodules"
-git submodule foreach --recursive 'git archive --prefix=$path/ HEAD | tar -x -C '${OUTPUT_DIR}
-
-# Checkout to the previous branch and update the submodules
-# to leave everything as it was before
-echo "Restoring repo state"
-git checkout ${CURRENT_BRANCH}
-git submodule update
-git stash pop
+git submodule foreach 'git archive --prefix=$path/ HEAD | tar -x -C '${OUTPUT_DIR}
