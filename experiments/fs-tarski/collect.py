83c1efa4b3c14053db79d4ee674c57d572a2e02f
Guillem Frances <guillem.frances@gmail.com>
2019-12-29 18:00:42 +0100

    Sample collection Lab script

--- experiments/fs-tarski/collect.py
+++ experiments/fs-tarski/collect.py
@@ -5,12 +5,16 @@ from common import ALL_ATTRIBUTES, BaseReport
 
 def main():
 
-    algos=['brfs-naive', 'brfs-sdd-asp', 'brfs-sdd-vo-asp']
-    algos=None
-
+    # Use the path where the combined results will be left:
     exp = Experiment('/homedtic/gfrances/projects/code/fs-sdd/experiments/data/combined')
-    exp.add_fetcher('/homedtic/gfrances/projects/code/fs-sdd/experiments/fs-tarski/data/fs-tarski-sat-eval', filter_algorithm=algos)
-    exp.add_fetcher('/homedtic/gfrances/projects/code/fs-sdd/experiments/fs-tarski/data/fs-tarski-sat_r-eval', filter_algorithm=algos)
+
+    # Use the paths where the results of the different experiments to be combined are:
+    exp.add_fetcher('/homedtic/gfrances/projects/code/fs-sdd/experiments/fs-tarski/data/fs-tarski-sat-eval')
+    exp.add_fetcher('/homedtic/gfrances/projects/code/fs-sdd/experiments/fs-tarski/data/fs-tarski-sat_r-eval')
+
+    # Use the filter_algorithm parameter if you only need some algos in the table, e.g.
+    # algos=['brfs-naive', 'brfs-sdd-asp', 'brfs-sdd-vo-asp']
+    # exp.add_fetcher('/homedtic/gfrances/projects/code/fs-sdd/experiments/fs-tarski/data/fs-tarski-sat-eval', filter_algorithm=algos)
 
     attributes = [att for name, att in ALL_ATTRIBUTES.items() if not name.startswith('sdd')]
     exp.add_report(
