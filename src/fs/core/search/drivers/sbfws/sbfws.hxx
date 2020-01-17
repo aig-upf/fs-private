b5c729422ad5d96453d2b3f9dddf01d74866216b
Guillem Frances <guillem.frances@gmail.com>
2020-01-17 12:31:24 +0100

    Disable BFWS search to get only IW(1) and IW(2) statistics

--- src/fs/core/search/drivers/sbfws/sbfws.hxx
+++ src/fs/core/search/drivers/sbfws/sbfws.hxx
@@ -422,9 +422,9 @@ public:
 
 
 		// Force one simulation from the root node and abort the search
-//        _heuristic.get_hash_r(*root);
+        _heuristic.get_hash_r(*root);
 //  		_heuristic.compute_R(*root);
-// 		return false;
+ 		return false;
 
 
 		// The main search loop
