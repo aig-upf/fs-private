44c608675cf113018a2cd44104cd3fb0a7f672dd
Guillem Frances <guillem.frances@gmail.com>
2020-01-29 12:00:34 +0100

    Generate latex reports for BFWS experiments

--- experiments/fs-tarski/summarize_bfws.py
+++ experiments/fs-tarski/summarize_bfws.py
@@ -1,7 +1,14 @@
 #! /usr/bin/env python
+import itertools
 import json
 from os import path
+import subprocess
 from statistics import mean
+
+import pandas as pd
+
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_columns', None)
 
 here = path.abspath(path.dirname(__file__))
 ALGORITHMS = {'5ba85646-bfws-r0': 'BFWS(R_0)',
@@ -16,13 +23,12 @@ def parse_json(filename):
 
 
 def print_results(results, name):
-    import pandas as pd
-
-    means = ['iw1_reached_subgoals', 'iw2_reached_subgoals', 'num_reach_actions', 'num_state_vars', 'total_simulation_time']
+    means = ['iw1_reached_subgoals', 'iw2_reached_subgoals', 'num_reach_actions', 'num_state_vars', 'total_simulation_time', 'iw_precondition_reachability']
     aggrs = list(ALGORITHMS.values())
 
     domains = sorted(set(k[0] for k in results.keys()))
     per_domain = []
+    per_instance = []
 
     with open(path.join(here, f'instances_{name}.txt'), 'w') as f:
         for d in domains:
@@ -34,8 +40,9 @@ def print_results(results, name):
                 summary[a] = sum(v[a] for v in instances)
             per_domain.append(summary)
 
-            print(f'\n\n{d}\n{pd.DataFrame(instances)}')
-            print(f'\n\n{d}\n{pd.DataFrame(instances)}', file=f)
+            # print(f'\n\n{d}\n{pd.DataFrame(instances)}')
+            # print(f'\n\n{d}\n{pd.DataFrame(instances)}', file=f)
+            per_instance.append((d, pd.DataFrame(instances)))
 
     total_summary = {'problem': 'Summary'}
     for a in means:
@@ -43,33 +50,34 @@ def print_results(results, name):
     for a in aggrs:
         total_summary[a] = sum(v[a] for v in per_domain)
 
-    with open(path.join(here, f'domains_{name}.txt'), 'w') as f:
-        print(f"\n\nAverages\n{pd.DataFrame(per_domain + [total_summary]).round(2)}")
-        print(f"\n\nAverages\n{pd.DataFrame(per_domain + [total_summary]).round(2)}", file=f)
+    return pd.DataFrame(per_domain + [total_summary]), per_instance
 
 
-def main():
-    collect_results_jcai17()
-    collect_results_ipc18()
+def generate_latex_report(content, filename):
+    with open(path.join(here, 'tpl.tex'), 'r') as f:
+        tpl = f.read()
+    output = path.join(here, filename)
+    with open(output, 'w') as f:
+        print(tpl.replace('#Content#', content), file=f)
+    subprocess.run(["ls", "-l"])
+    subprocess.run(f"pdflatex {filename}".split(),  check=True)
+
+
+def generate_latex_section(title, content):
+    return f'\\section{{{title}}}\n\n{content}'
+
+
+def generate_latex_pagebreak():
+    return f'\\newpage'
 
 
 def collect_results_ipc18():
     results = dict()  # We'll leave all results here
 
     # Collect some data about IW simulations and problem stats alone
-    data = parse_json(path.join(here, 'data/fs-tarski-iws_ipc18-eval/properties'))
+    data = parse_json(path.join(here, 'data/fs-tarski-prec_reachab_ipc18-eval/properties'))
     for _, run in data.items():
-        if run['algorithm'] != '1d8325d7-bfws-iw2-mt_reach':  # We'll take the data from the run with reachability
-            continue
-        problem_id = (run['domain'], run['problem'])
-        results[problem_id] = {
-            'problem': run['problem'],
-            'iw1_reached_subgoals': run['iw1_reached_subgoals'],
-            'iw2_reached_subgoals': run['iw2_reached_subgoals'],
-            'num_reach_actions': run['num_reach_actions'],
-            'num_state_vars': run['num_state_vars'],
-            'total_simulation_time': run['total_simulation_time'],
-        }
+        copy_run_data(run, results)
 
     # Collect data about BFWS performance
     data = parse_json(path.join(here, 'data/fs-tarski-bfws_stats_ipc18-eval/properties'))
@@ -83,24 +91,17 @@ def collect_results_ipc18():
         algo = ALGORITHMS[run['algorithm']]
         results[problem_id][algo] = int('coverage' in run and run['coverage'] == 1)
 
-    print_results(results, "ipc18")
+    return print_results(results, "ipc18")
 
 
 def collect_results_jcai17():
     results = dict()  # We'll leave all results here
 
     # Collect some data about IW simulations and problem stats alone
-    data = parse_json(path.join(here, 'data/fs-tarski-iws_ijcai17-eval/properties'))
+    # data = parse_json(path.join(here, 'data/fs-tarski-iws_ijcai17-eval/properties'))
+    data = parse_json(path.join(here, 'data/fs-tarski-prec_reachab_ijcai17-eval/properties'))
     for _, run in data.items():
-        problem_id = (run['domain'], run['problem'])
-        results[problem_id] = {
-            'problem': run['problem'],
-            'iw1_reached_subgoals': run['iw1_reached_subgoals'],
-            'iw2_reached_subgoals': run['iw2_reached_subgoals'],
-            'num_reach_actions': run['num_reach_actions'],
-            'num_state_vars': run['num_state_vars'],
-            'total_simulation_time': run['total_simulation_time'],
-        }
+        copy_run_data(run, results)
 
     data = parse_json(path.join(here, 'data/fs-tarski-bfws_stats_ijcai17_rall-eval/properties'))
     data.update(parse_json(path.join(here, 'data/fs-tarski-bfws_stats_ijcai17_r0-eval/properties')))
@@ -111,7 +112,35 @@ def collect_results_jcai17():
         algo = ALGORITHMS[run['algorithm']]
         results[problem_id][algo] = int('coverage' in run and run['coverage'] == 1)
 
-    print_results(results, "ijcai17")
+    return print_results(results, "ijcai17")
+
+
+def copy_run_data(run, results):
+    problem_id = (run['domain'], run['problem'])
+    results[problem_id] = {
+        'problem': run['problem'],
+        'iw1_reached_subgoals': run['iw1_reached_subgoals'],
+        'iw2_reached_subgoals': run['iw2_reached_subgoals'],
+        'num_reach_actions': run['num_reach_actions'],
+        'num_state_vars': run['num_state_vars'],
+        'total_simulation_time': run['total_simulation_time'],
+        'iw_precondition_reachability': run['iw_precondition_reachability'],
+    }
+
+
+def main():
+    ijcai17_dom, ijcai17_inst = collect_results_jcai17()
+    ipc18_dom, ipc18_inst = collect_results_ipc18()
+    content = ''
+    content += generate_latex_section("IJCAI 17 Benchmarks", ijcai17_dom.round(2).to_latex())
+    content += generate_latex_pagebreak()
+
+    content += generate_latex_section("IPC 18 Benchmarks", ipc18_dom.round(2).to_latex())
+    content += generate_latex_pagebreak()
+
+    for dom, instance in itertools.chain(ijcai17_inst, ipc18_inst):
+        content += generate_latex_section(dom, instance.round(2).to_latex())
+    generate_latex_report(content, 'bfws-analysis.tex')
 
 
 if __name__ == '__main__':
