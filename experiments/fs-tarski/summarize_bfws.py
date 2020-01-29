3ba8457a90905b0ebfb28ee34b48431e034b733c
Guillem Frances <guillem.frances@gmail.com>
2020-01-29 10:33:24 +0100

    Improve BFWS experiment scripts

--- experiments/fs-tarski/summarize_bfws.py
+++ experiments/fs-tarski/summarize_bfws.py
@@ -9,7 +9,13 @@ ALGORITHMS = {'5ba85646-bfws-r0': 'BFWS(R_0)',
               '5ba85646-bfws-rg': 'BFWS(R_G)'}
 
 
-def print_results(results):
+def parse_json(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def print_results(results, name):
     import pandas as pd
 
     means = ['iw1_reached_subgoals', 'iw2_reached_subgoals', 'num_reach_actions', 'num_state_vars', 'total_simulation_time']
@@ -18,7 +24,7 @@ def print_results(results):
     domains = sorted(set(k[0] for k in results.keys()))
     per_domain = []
 
-    with open(path.join(here, 'instances.txt'), 'w') as f:
+    with open(path.join(here, f'instances_{name}.txt'), 'w') as f:
         for d in domains:
             instances = [v for k, v in results.items() if k[0] == d]
             summary = {'problem': d}
@@ -37,18 +43,21 @@ def print_results(results):
     for a in aggrs:
         total_summary[a] = sum(v[a] for v in per_domain)
 
-    with open(path.join(here, 'domains.txt'), 'w') as f:
+    with open(path.join(here, f'domains_{name}.txt'), 'w') as f:
         print(f"\n\nAverages\n{pd.DataFrame(per_domain + [total_summary]).round(2)}")
         print(f"\n\nAverages\n{pd.DataFrame(per_domain + [total_summary]).round(2)}", file=f)
 
 
 def main():
+    collect_results_jcai17()
+    collect_results_ipc18()
+
+
+def collect_results_ipc18():
     results = dict()  # We'll leave all results here
 
     # Collect some data about IW simulations and problem stats alone
-    with open(path.join(here, 'data/fs-tarski-iws_ipc20-eval/properties'), 'r') as f:
-        data = json.load(f)
-
+    data = parse_json(path.join(here, 'data/fs-tarski-iws_ipc18-eval/properties'))
     for _, run in data.items():
         if run['algorithm'] != '1d8325d7-bfws-iw2-mt_reach':  # We'll take the data from the run with reachability
             continue
@@ -63,20 +72,46 @@ def main():
         }
 
     # Collect data about BFWS performance
-    # fs-tarski-bfws_stats_ipc20_rall-eval
-    with open(path.join(here, 'data/fs-tarski-bfws_stats_ipc20-eval/properties'), 'r') as f:
-        data = json.load(f)
+    data = parse_json(path.join(here, 'data/fs-tarski-bfws_stats_ipc18-eval/properties'))
     # Discard results from buggy r_all configuration:
     data = {k: v for k, v in data.items() if '5ba85646-bfws-rall' not in k}
-    with open(path.join(here, 'data/fs-tarski-bfws_stats_ipc20_rall-eval/properties'), 'r') as f:
-        data = {**data, **json.load(f)}  # overwrite the previous dict
+
+    data.update(parse_json(path.join(here, 'data/fs-tarski-bfws_stats_ipc18_rall-eval/properties')))
 
     for _, run in data.items():
         problem_id = (run['domain'], run['problem'])
         algo = ALGORITHMS[run['algorithm']]
         results[problem_id][algo] = int('coverage' in run and run['coverage'] == 1)
 
-    print_results(results)
+    print_results(results, "ipc18")
+
+
+def collect_results_jcai17():
+    results = dict()  # We'll leave all results here
+
+    # Collect some data about IW simulations and problem stats alone
+    data = parse_json(path.join(here, 'data/fs-tarski-iws_ijcai17-eval/properties'))
+    for _, run in data.items():
+        problem_id = (run['domain'], run['problem'])
+        results[problem_id] = {
+            'problem': run['problem'],
+            'iw1_reached_subgoals': run['iw1_reached_subgoals'],
+            'iw2_reached_subgoals': run['iw2_reached_subgoals'],
+            'num_reach_actions': run['num_reach_actions'],
+            'num_state_vars': run['num_state_vars'],
+            'total_simulation_time': run['total_simulation_time'],
+        }
+
+    data = parse_json(path.join(here, 'data/fs-tarski-bfws_stats_ijcai17_rall-eval/properties'))
+    data.update(parse_json(path.join(here, 'data/fs-tarski-bfws_stats_ijcai17_r0-eval/properties')))
+    data.update(parse_json(path.join(here, 'data/fs-tarski-bfws_stats_ijcai17_rg-eval/properties')))
+
+    for _, run in data.items():
+        problem_id = (run['domain'], run['problem'])
+        algo = ALGORITHMS[run['algorithm']]
+        results[problem_id][algo] = int('coverage' in run and run['coverage'] == 1)
+
+    print_results(results, "ijcai17")
 
 
 if __name__ == '__main__':
