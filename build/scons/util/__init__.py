29181f94a2d76151af9776439ff02bc526e7e9b4
Guillem Frances <guillem.frances@gmail.com>
2020-06-12 16:02:16 +0200

    Fix python imports

--- build/scons/util/__init__.py
+++ build/scons/util/__init__.py
@@ -1,5 +1,5 @@
 
 # MRJ: imports the functions in the module into the
 # the module/namespace util.build
-from _scons_helper import *
-from _pkg_config import *
+from ._scons_helper import *
+from ._pkg_config import *
